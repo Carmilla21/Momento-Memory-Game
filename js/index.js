8fcc6bf109f14cd0f5c469e965c86a2fa439ea7e
Mitch Rhodes <M.Rhodes87@comcast.net>
2021-03-10 12:16:14 -0500

    changed stop to pause in html. Added the countdown timer in js along with the button changing text from start to pause. Actual pausing not implemented.

--- js/index.js
+++ js/index.js
@@ -2,15 +2,41 @@
     document.getElementById("start").addEventListener("click", () => {
         /*needs to get input info for timer and put it into the countdown timer.
         It will also allow the cards to start being moved. Then the start button
-        will become a stop button and it's function changes. 
+        will become a pause button and it's function changes. 
         
-        The stop button needs to stop the timer at its current position and stop
+        The pause button needs to stop the timer at its current position and stop
         the player from moving the cards. The button while stopped will turn back 
         to start.
 
         Start can't be pressed if there is no time amount for the timer.
 
         The start button can not reset the timer or cards.*/
+
+        //grabs info and creates a "timer". Not actual timer yet.
+        let userTime = document.getElementById("seconds").value;
+        const countdown = document.createElement("p");
+
+        countdown.innerText = userTime;
+
+        const timer = document.querySelector(".timer");
+
+        timer.append(countdown);
+
+        //Start button becomes a pause button
+        let start = document.getElementById("start");
+        let interval = -1; //indicates timer is paused
+
+        if (interval === -1) {
+            start.innerText = "Pause";
+            interval = setInterval(function () {
+                userTime--;
+                countdown.innerText = userTime;
+            }, 1000);
+        } else {
+            start.innerText = "Start";
+            clearInterval(interval);
+            interval = -1;
+        };
     });
 
     document.getElementById("reset").addEventListener("click", () => {
