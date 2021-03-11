7fcb8be24dd996c10c51672c99dcda4482853626
Mechial <mechial.harrison@gmail.com>
2021-03-10 19:41:14 -0500

    added event listener to flip cards when clicked

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 (function () {
-    document.getElementById("start").addEventListener("click", () => {
-        /*needs to get input info for timer and put it into the countdown timer.
+  document.getElementById("start").addEventListener("click", () => {
+    /*needs to get input info for timer and put it into the countdown timer.
         It will also allow the cards to start being moved. Then the start button
         will become a pause button and it's function changes. 
         
@@ -12,36 +12,45 @@
 
         The start button can not reset the timer or cards.*/
 
-        //create a timer that counts up how long it took.
-        const timer = document.querySelector(".timer");
-        let completeTime = 0;
+    //create a timer that counts up how long it took.
+    const timer = document.querySelector(".timer");
+    let completeTime = 0;
 
+    timer.innerText = completeTime;
+
+    //Start button becomes a pause button
+    let start = document.getElementById("start");
+    let interval = -1; //indicates timer is paused
+
+    //maybe do an add classlist and remove classlist for start and pause. have only start in HTML to begin with
+    if (interval === -1) {
+      start.innerText = "Pause";
+      interval = setInterval(function () {
+        completeTime++;
         timer.innerText = completeTime;
+      }, 1000);
+    } else {
+      start.innerText = "Start";
+      clearInterval(interval);
+      interval = -1;
+    }
+  });
 
-
-
-        //Start button becomes a pause button
-        let start = document.getElementById("start");
-        let interval = -1; //indicates timer is paused
-
-
-        //maybe do an add classlist and remove classlist for start and pause. have only start in HTML to begin with
-        if (interval === -1) {
-            start.innerText = "Pause";
-            interval = setInterval(function () {
-                completeTime++;
-                timer.innerText = completeTime;
-            }, 1000);
-        } else {
-            start.innerText = "Start";
-            clearInterval(interval);
-            interval = -1;
-        };
-    });
-
-    document.getElementById("reset").addEventListener("click", () => {
-        /*The reset button, when pressed, will randomize the cards and reset 
+  document.getElementById("reset").addEventListener("click", () => {
+    /*The reset button, when pressed, will randomize the cards and reset 
         the clock to have the timer ask for the input amount. It also makes the
         start button reappear if it was in motion.*/
-    });
+  });
+
+  const cards = document.querySelectorAll(".card");
+
+  /**
+   * Function will add the flip class to element
+   */
+  function flipCard() {
+    this.classList.toggle("flip");
+  }
+
+  //Adds event listener to all cards to flip
+  cards.forEach((card) => card.addEventListener("click", flipCard));
 })();
