afe86efa517aac6582b4757a1aeac1bc496c7acc
Mechial <mechial.harrison@gmail.com>
2021-03-11 01:50:15 -0500

    Matching logic added. Requires debugging.

--- js/index.js
+++ js/index.js
@@ -45,11 +45,16 @@
   const cards = document.querySelectorAll(".card");
 
   let hasFlippedCard = false;
-  let firstCard, secondCard;
+  let locked = false;
+  let firstCard = null;
+  let secondCard = null;
   /**
    * Function will add the flip class to element
    */
   function flipCard() {
+    if (locked) return;
+    if (this === firstCard) return;
+
     this.classList.add("flip");
 
     if (!hasFlippedCard) {
@@ -59,30 +64,45 @@
     }
 
     secondCard = this;
-    hasFlippedCard = false;
 
-    checkMatch();
+    matched();
   }
 
   //Checks if two cards match
-  let matched =
-    firstCard.dataset.card - type === secondCard.dataset.card - type;
-  matched ? disablecards() : unflipCards();
+  function matched() {
+    let matched = firstCard.dataset.cardPair === secondCard.dataset.cardPair;
+    matched ? disableCards() : unFlip();
+  }
 
   //disables attemted cards till
-  function disablecards() {
+  function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
-    firstCard.style.display = "none";
-    secondCard.style.display = "none";
+
+    setTimeout(() => {
+      firstCard.style.display = "none";
+      secondCard.style.display = "none";
+
+      resetCards();
+    }, 1500);
   }
 
   //Unflips mismatched cards after 1.5sec
   function unFlip() {
+    locked = true;
+
     setTimeout(() => {
       firstCard.classList.remove("flip");
       secondCard.classList.remove("flip");
+
+      resetCards();
     }, 1500);
+  }
+
+  //Reset card variables
+  function resetCards() {
+    [hasFlippedCard, locked] = [false, false];
+    [firstCard, secondCard] = [null, null];
   }
   //Adds event listener to all cards to flip
   cards.forEach((card) => card.addEventListener("click", flipCard));
